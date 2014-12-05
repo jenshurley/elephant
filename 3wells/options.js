b61b8e91f66d483c9f97d54040134364fd55e77d
Jen Shurley <jenshurley@gmail.com>
2014-12-05 10:26:04 -0700

    implemented some Maferial Design guidelines in index, removed scratch code from options.js

--- 3wells/options.js
+++ 3wells/options.js
@@ -5,35 +5,13 @@
 
 
 $(document).ready(function() {
-    //var myTask = getRandomTask();
-    //var wells = $('.well');
-    //var data = ['weeds', 'handles', 'sinks', 'table', 'desk'];
-
-    //$('.well').each(function(index, element){
-    //    getRandomTask(data);
-    //
-    //});
-
-    //$('.tasks').click(function(){
-    //    console.log('clicked');
-    //    $('.msg1').append(getRandomTask(data));
-    //    //$('.msg1' '.msg2' '.msg3').append(getRandomTask(data));
-    //    //$('.msg2').getRandomTask(data);
-    //    //$('.msg3').getRandomTask(data);
-    //});
-
-    //function (){
-        //var data = ['weeds', 'handles', 'sinks', 'table', 'desk'];
+        //global variable of the different json arrays
         var wells = $('.well');
 
         wells.each(function(){
             $(this).text(getRandomTask(data))
             });
 
-
-    //$(wells).each(function(){
-    //    getRandomTask(data)
-    //});
 
 
     function getRandomTask() {
@@ -52,58 +30,3 @@ $(document).ready(function() {
     };
 
 });
-
-//
-//function renderWells(wells){
-//    for (var i = 0; i <2; i++) {
-//        console.log("renderWells");
-//
-//        if (i ===0) {
-//            $("#msg1").append(randomTask);
-//        } else
-//        if (i===1){
-//            $("#msg2").append(randomTask);
-//        } else {
-//            $("#msg3").append(randomTask);
-//        }
-//    }
-//
-//    $.each(wells, getRandomTask(data);
-//{
-    //    $("#msg1").append(randomTask);
-    //    $("#msg2").append(randomTask);
-    //    $("#msg3").append(randomTask);
-    //});
-
-//});
-//
-//};
-//
-
-
-//    var html;
-//
-//    html = "<h3>Do this</h3>";
-//
-//
-//    data.forEach(function (employee, index) {
-//
-//        html += "<tr>";
-//        html += "<td>" + employee.name + "</td>";
-//        html += "<td>" + employee.phone + "</td>";
-//        html += "<td>" + employee.address + "</td>";
-//        html += "<td><button type='button' class='btn btn-default btn-sm delete' index='" + index + "'>Del</button></td>";
-//        html += "<td><button type='button' class='btn btn-default btn-sm edit' index='" + index + "'>Edit</button></td>";
-//        html += "<td><button type='button' class='btn btn-default btn-sm map' index='" + index + "'>Map</button></td>";
-//        html += "</tr>";
-//    })
-//
-//
-//    html += "</table>";
-//
-//    $("#employee_list").html(html);
-//};
-
-/**
- * Created by Jen on 11/26/2014.
- */
