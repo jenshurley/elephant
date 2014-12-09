ee582b985ffba4e84b44228740df43bb64b07c4a
Jen Shurley <jenshurley@gmail.com>
2014-12-08 19:17:03 -0700

    Fixed the issue of replacement by using _.shuffle and _.sample

--- 3wells/options.js
+++ 3wells/options.js
@@ -1,8 +1,6 @@
 //TEXT SELECTIONS
 //for each well, choose a different random array item (no replacement)]
 
-//var data = ['weeds', 'handles', 'sinks', 'table', 'desk'];
-
 
 $(document).ready(function() {
         //global variable of the different json arrays
@@ -12,21 +10,41 @@ $(document).ready(function() {
             $(this).text(getRandomTask(data))
             });
 
+        //submit.on('click',function(){
+        //    getRandomTask();
+        //
+    //todo reset button
+
 
 
     function getRandomTask() {
-        var min = 0;//i know. not how you do this.
-        var max = data.length - 1;
-            console.log(max);
-        var randomIndex = Math.floor((Math.random() * (max - min)) + min);
-            console.log(randomIndex);
-        var task = data[randomIndex];
+        //var input = [];
+        var input = data;
+        //get input from list of arrays in json object
 
-            console.log(task);
-        return task;
+        var shuffled = _.shuffle(input);
+        //how do i chain this??
+
+        var getThree = _.sample(shuffled);
+
+        return getThree;
+
+    };
+
+
+    //function getRandomTask() {
+    //    var min = 0;
+    //    var max = data.length - 1;
+    //        console.log(max);
+    //    var randomIndex = Math.floor((Math.random() * (max - min)) + min);
+    //        console.log(randomIndex);
+    //    var task = data[randomIndex];
+    //
+    //        console.log(task);
+    //    return task;
 
         //$(".msg").append(task);
 
-    };
+    //};
 
 });
