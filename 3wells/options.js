f22c94adb2f6de183673f47f1ecaf3c51130d77a
Jen Shurley <jenshurley@gmail.com>
2014-12-12 23:04:37 -0700

    Added input box

--- 3wells/options.js
+++ 3wells/options.js
@@ -4,29 +4,128 @@
 
 $(document).ready(function() {
         //global variable of the different json arrays
+    var table = $('#lists');
+    var tbody = table.find('tbody');
 
-    var choices = $('.btn'); //this reflects the choice of list as the buttons present them
+    var listField= $('#listName');
+    var itemField = $('#item');
+    var addButton = $('#add');
+    var clearAll = $('#clearAll'); //todo add this button
+
+    // Check to see if we have existing records
+    var wellsLists = localStorage.getItem('wellsLists');
+    wellsLists = JSON.parse(wellsLists);
+
+
+    var choices = $('.btn'); //choice of list as the buttons present them
     var cleanBtn = $('#clean');
     var makeBtn = $('#make');
     var openBtn =$('#open');
 
     var wells = $('.well'); //selects the bootstrap wells
 
+    $('#tableContainer').hide();
+
+    addButton.click(function() {
+        console.log('clicked!');
+        $('#tableContainer').slideDown().show();
+        $('#listChoice').text(' > Add New Items');
+        });//adds the table for entering new items
+//todo fix the slide. looks weird.
+
     choices.on('mouseenter',function(){
         console.log('m.e.');
         $(this).removeClass('btn btn-default').addClass('btn btn-info');
         //this is the box where the mouse has entered
         });
-    //todo fix the propogation issue with button group - reset and remove active class
+
 
     choices.on('mouseleave',function(){
         $(this).removeClass('btn btn-info').addClass('btn btn-default')
 
          });
 
-    //cleanBtn.on('click', constructAssa);
+    //cleanBtn.on('click', getArray);
+
+
+    //makeBtn.on('click', constructArray);
+
+    //openBtn.on('click', )
 
     choices.on('click', getRandomTask);
+
+
+    if(!wellsLists){
+        wellsLists = [];
+    } else {
+        wellsLists.forEach(function(record){
+            tbody.append( makeRow(record) );
+        });
+    }
+
+
+    addButton.on('click', function(){
+
+        var list = listField.val();
+        var item = itemField.val();
+
+        if(list == 'clean' || list == 'make' || list == 'open'){
+            var record = {
+                list: list,
+                item: item,
+                ts: Date.now()
+            };
+
+            wellsLists.push(record);
+
+            save();
+
+            tbody.append( makeRow(record) );
+    //todo fix this -- row not being appended. also console log localStorage get to see if ...
+            listField.val("");
+            itemField.val("").css({borderColor: ''});
+
+
+            } else {
+            listField.css({
+                borderColor: 'red'
+            });
+        }
+
+    });
+
+
+    tbody.on('click', 'button.remove', function(event){
+        var row = $(this).parents('tr');
+        var id = row.data('id');
+
+        wellsLists = wellsLists.filter(function(record){
+            return (record.ts !== id);
+        });
+
+        save();
+
+        row.remove();
+    });
+
+    clearAll.on('click', function(){
+        tbody.empty();
+        wellsLists = [];
+        save();
+    });
+
+    function save(){
+        localStorage.setItem('wellsLists', JSON.stringify(wellsLists));
+    }
+
+    function makeRow(record){
+        return "<tr data-id='"+ record.ts +"'>" +
+            "<td>" + record.list + "</td>" +
+            "<td>" + record.item + "</td>" +
+            "<td><button class='remove btn btn-primary btn-xs'>remove</button></td>" +
+            "</tr>";
+    }
+
 
 
 
@@ -82,7 +181,4 @@ $(document).ready(function() {
         //
         //}//function reset
 
-
-
-
-});
+});//document ready
